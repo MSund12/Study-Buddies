58f61a2f64624a14c283aaa4a1aeb2681ed4eaad
MSund12 <lukaszn215@protonmail.com>
2025-03-07 23:55:33 -0500

    fixed connection issues

--- study_buddies/server/config/db.js
+++ study_buddies/server/config/db.js
@@ -4,7 +4,11 @@ const connectDB = async () => {
   try {
     // Replace with your MongoDB connection string
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost:27017/myapp",
+      process.env.MONGO_URI || "mongodb+srv://yorkstudygroupfinder:QjdeoOxa2RERs5Ea@studygroupfinder.08k9b.mongodb.net/",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
