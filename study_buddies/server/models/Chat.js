37d161da9b110eaea90fb760e0a75e066e945ab7
hrs201 <hrs201@my.yorku.ca>
2025-03-27 16:54:02 -0400

    Updated Chat.js
    feat: Add ChatMessage schema and model for storing chat data.

---
+++ study_buddies/server/models/Chat.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const chatSchema = new mongoose.Schema({
+  sender: { type: String, required: true },
+  receiver: { type: String, required: true },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+  chatId: {type: String, required: true, index: true}
+}, { timestamps: true });
+
+const ChatMessage = mongoose.model('Chat', chatSchema);
+
+export default Chat;
